880ae4393241e46973fcdc1183882dd18ee91dbc
tasuku <ctake1235@gmail.com>
2022-09-02 11:08:54 +0900

    ゲーミングLED完結

--- ledcheck.cpp
+++ ledcheck.cpp
@@ -4,11 +4,11 @@ PwmOut green(D3),red(D5),blue(D6);
 double PI=3.141592;
 int main(){
     while (true) {
-        for(double i=0;;i+=0.1){
-            green=sin(i)*10;
-            red=sin(2);
-            blue=sin(i)*10;
-            wait_us(50000);
+        for(double i=0;;i++){
+            green=0.5*sin(i*PI/180)+0.5;
+            red=0.5*sin((i+120)*PI/180)+0.5;
+            blue=0.5*sin((i+240)*PI/180)+0.5;
+            wait_us(8000);
         }
     }
 }
