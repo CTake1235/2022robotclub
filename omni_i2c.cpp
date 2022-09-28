395a37d820f2b0b9c9eccf08ba84e1bb45f37762
tasuku <ctake1235@gmail.com>
2022-09-28 18:29:28 +0900

    change

--- omni_i2c.cpp
+++ omni_i2c.cpp
@@ -2,8 +2,8 @@
 //clockwは時計回り、anticwは反時計回り
 
 #include "mbed.h"
-#include"PS3.h"
-#include"mbed_wait_api.h"
+#include "PS3.h"
+#include "mbed_wait_api.h"
 #include <stdexcept>
 #include "QEI.h"
 #include "time.h"
@@ -12,148 +12,118 @@
 #define migiMD 0x50
 #define sitaMD 0x70
 #define hidariMD 0x10
-I2C i2c (D14,D15);
-PS3 ps3 (A0,A1);
-DigitalOut sig(D13);//緊急停止用
-QEI encoder( D9, D10, NC, 2048, QEI::X2_ENCODING);
+I2C              i2c(D14,D15);
+DigitalOut       sig(D13);//緊急停止用
+QEI              encoder( D9, D10, NC, 2048, QEI::X2_ENCODING);
 //QEI 任意の名前( A相のピン, B相のピン, Z相のピン, 分解能, 逓倍);
-sonMD right(D3,NC,0.00015);
-sonMD left(D4,NC,0.00015);
-sonMD reload(D5,NC,0.00015);
-DigitalOut green(D6),red(D7),blue(D8);
-int select,start,ue,migi,sita,hidari,L1,R1,sankaku,batu,maru,sikaku;
-void getdata(void);
-int send(char add,char dat);
-void autorun(void);//中央を自動でとる
+sonMD            right(D3,NC,0.00015);
+sonMD            left(D4,NC,0.00015);
+sonMD            reload(D5,NC,0.00015);
+DigitalOut       green(D6), red(D7), blue(D8);
+UnbufferedSerial raspi(D0,D1,9600);
+
+void send(char add,char dat);
+void autorun(int raspi_dat);//中央を自動でとる
 void led_enable(void);
 
 int main(){
-    char clockw=0xc9;
-    char anticw=0x36;
-    char sb=0x80;//ショートブレーキ用
-    sig=0;
+    char clockw = 0xc9;
+    char anticw = 0x36;
+    static char data;
+    int res;
+    int autorun_state;
+    char sb = 0x80;//ショートブレーキ用
+    sig = 0;
     while (true) {
         led_enable();
-        getdata();
-        if(select == 1){
-            sig=1;
-        }
-        else if(start == 1){
-            sig=0;
-        }
-        else if(sankaku == 1){
-            autorun();
-        }
-        else if(ue == 1){
-            send(ueMD,sb);
-            send(migiMD,anticw);
-            send(sitaMD,sb);
-            send(hidariMD,clockw);
-        }
-        else if(migi == 1){
-            send(ueMD,clockw);
-            send(migiMD,sb);
-            send(sitaMD,anticw);
-            send(hidariMD,sb);
-        }
-        else if(sita == 1){
-            send(ueMD,sb);
-            send(migiMD,clockw);
-            send(sitaMD,sb);
-            send(hidariMD,anticw);
-        }
-        else if(hidari == 1){
-            send(ueMD,anticw);
-            send(migiMD,sb);
-            send(sitaMD,clockw);
-            send(hidariMD,sb);
-        }
-        else if(R1 == 1){
-            send(ueMD,clockw);
-            send(migiMD,clockw);
-            send(sitaMD,clockw);
-            send(hidariMD,clockw);
-        }
-        else if(L1 == 1){
-            send(ueMD,anticw);
-            send(migiMD,anticw);
-            send(sitaMD,anticw);
-            send(hidariMD,anticw);
-        }
-        else if(maru == 1){
-            right.move_p1(0.02,0.5);
-            left.move_p1(0.02,0.5);
-        }
-        else if(sikaku == 1){
-            reload.move_p1(0.02,0.2);
-        }
-        else{
-            send(ueMD,sb);
-            send(migiMD,sb);
-            send(sitaMD,sb);
-            send(hidariMD,sb); 
-
-            right.stop();
-            left.stop();
-            reload.stop();
+        raspi.read(&data,9);
+        if(res == 1){
+            switch(data){
+                case 1://starting autorun(option)
+                    autorun(data);
+                    break;
+                case 2:
+                    autorun(data);
+                    break;
+                case 3://move to ue
+                    send(ueMD,sb);
+                    send(migiMD,anticw);
+                    send(sitaMD,sb);
+                    send(hidariMD,clockw);
+                    break;
+                case 4://move to migi 
+                    send(ueMD,clockw);
+                    send(migiMD,sb);
+                    send(sitaMD,anticw);
+                    send(hidariMD,sb); 
+                    break;
+                case 5://move to sita
+                    send(ueMD,sb);
+                    send(migiMD,clockw);
+                    send(sitaMD,sb);
+                    send(hidariMD,anticw);
+                    break;
+                case 6://move to hidari
+                    send(ueMD,anticw);
+                    send(migiMD,sb);
+                    send(sitaMD,clockw);
+                    send(hidariMD,sb);
+                    break;
+                case 7://turn clockwise
+                    send(ueMD,clockw);
+                    send(migiMD,clockw);
+                    send(sitaMD,clockw);
+                    send(hidariMD,clockw);
+                    break;
+                case 8://turn anti_clockwise
+                    send(ueMD,anticw);
+                    send(migiMD,anticw);
+                    send(sitaMD,anticw);
+                    send(hidariMD,anticw);
+                    break;
+                default:
+                    send(ueMD,sb);
+                    send(migiMD,sb);
+                    send(sitaMD,sb);
+                    send(hidariMD,sb); 
+                    break;
+            }
         }
     }
 }
-void getdata(void){
-    select = ps3.getSELECTState();
-    start = ps3.getSTARTState();
 
-    ue = ps3.getButtonState(PS3::ue);
-    migi = ps3.getButtonState(PS3::migi);
-    sita = ps3.getButtonState(PS3::sita);
-    hidari = ps3.getButtonState(PS3::hidari);
-
-    sankaku = ps3.getButtonState(PS3::sankaku);
-    batu = ps3.getButtonState(PS3::batu);
-
-    R1 = ps3.getButtonState(PS3::R1);
-    L1 = ps3.getButtonState(PS3::L1);
-
-    maru = ps3.getButtonState(PS3::maru);
-    sikaku = ps3.getButtonState(PS3::sikaku);
-}
-
-int send(char add,char dat){  
+void send(char add,char dat){  
     i2c.start();
     i2c.write(add);
     i2c.write(dat);
     i2c.stop();
-    wait_us(50000);
-    return 0;
+    wait_us(25000);
 }
 
-void autorun(void){
+void autorun(int raspi_dat){
     clock_t cstart,cend;
     double time,rpm;
+    int start;
     cstart = clock();
     encoder.reset();
     int pulse=0;
     while(true){
-        getdata();
         pulse = encoder.getPulses();
         printf("%d\n",pulse);
-        if(pulse >= 31403 || pulse <= -31403){//数値はあとで計測する！！！
-            break;
+        switch (raspi_dat) {
+            case 1:
+                if(pulse <= 31403 || pulse >= -31403){//数値はあとで計測する！！！
+                send(migiMD,0xf0);
+                send(hidariMD,0x0f);
+                cend = clock();
+                time = cend - start;
+                rpm = (pulse/4096)*(time*60/CLOCKS_PER_SEC);
+                //pulse/4096で回転数、time/CLOCKS_PER_SECで秒、60をかけて毎分になおす
+                printf("%lf",rpm);
+                }
+            case 2:
+                break;
         }
-        else if(batu == 1){
-            pulse=0;
-            break;
-        }
-        else{
-            send(migiMD,0xf0);
-            send(hidariMD,0x0f);
-            cend = clock();
-            time = cend - start;
-            rpm = (pulse/4096)*(time*60/CLOCKS_PER_SEC);
-            //pulse/4096で回転数、time/CLOCKS_PER_SECで秒、60をかけて毎分になおす
-            printf("%lf",rpm);
-        }
-        wait_us(20000);
     }
 }
 
