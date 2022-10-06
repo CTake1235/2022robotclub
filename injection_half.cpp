90d4f80b1b29d0c02558d5422fa56a8d096cd061
tasuku <ctake1235@gmail.com>
2022-10-06 17:14:24 +0900

    uhu~

--- injection_half.cpp
+++ injection_half.cpp
@@ -1,20 +1,60 @@
 #include "mbed.h"
 #include "sonMD.h"
+#define wingPWadd 0x58
 
-sonMD inside(D3,NC,0.00015);
-sonMD outside(D4,NC,0.00015);
-I2C i2c(D14,D15);
+sonMD            inside(D3,NC,0.00015);
+sonMD            outside(D4,NC,0.00015);
+sonMD            inside_reload(D5,NC,0.00015);
+sonMD            outside_reload(D6,NC,0.00015);
+UnbufferedSerial raspi(D0,D1,9600);
+I2C              i2c(D14,D15);
+DigitalIn        uelimitswitch(D5);
+DigitalIn        sitalimitswitch(D6);
 
-int send(char add, char dat);
+void send(char add, char dat);
 
 int main(){
+    int res;
+    static char data;
     while (true) {
-       switch (/*受け取った変数*/)
-        case 1:
+        res = raspi.read(&data,9);
+        if(res == 1){
+            switch (int(data)){
+                case 7://L1,open wing
+                    if(uelimitswitch == 0){
+                        send(wingPWadd,0xff);
+                    }
+                    break;
+                case 8://L2,close wing
+                    if(sitalimitswitch == 0){
+                        send(wingPWadd,0x00);
+                    }
+                    break;
+                case 9://maru,inject
+                    inside.move_p1(0.02,0.7);
+                    outside.move_p1(0.02,0.7);
+                    break;
+                case 10://sikaku,reload back
+                    inside.move_p2(0.02,0.1);
+                    outside.move_p2(0.02,0.1);
+                    break;
+                case 11://sankaku,reload go
+                    inside_reload.move_p1(0.02,0.1);
+                    outside_reload.move_p1(0.02,0.1);
+                    break;
+                default:
+                    inside.stop();
+                    outside.stop();
+                    inside_reload.stop();
+                    outside_reload.stop();
+                    send(wingPWadd,0x80);
+                    break;
+            }
+        }
     }
 }
 
-int send(char add, char dat){
+void send(char add, char dat){
     i2c.start();
     i2c.write(add);
     i2c.write(dat);
