8ff77870e7a00d45d898da6cb0f1820505decea3
tasuku <ctake1235@gmail.com>
2022-10-13 20:22:10 +0900

    change button status

--- injection_half_PW.cpp
+++ injection_half_PW.cpp
@@ -1,33 +1,86 @@
 #include "mbed.h"
 #include "sonMD.h"
+#include <cstdio>
+#include "time.h"
+
 #define wingPWadd 0x58
-#define shotpower 0.5
 
 UnbufferedSerial raspi(PA_0,PA_1,9600);
-sonMD            inside(PA_6,PB_6,0.00015);
-sonMD            outside(PA_9,PA_8,0.00015);
-sonMD            inside_reload(PB_9,PB_8,0.00015);
-sonMD            outside_reload(PC_9,PC_8,0.00015);
-sonMD            leg(PA_11,PB_2,0.00015);
-sonMD            leg_reload(PA_10,PC_8,0.00015);
-I2C              i2c(PC_7,PC_6);
-DigitalIn        uelimitswitch(PC_1);
+
+I2C              i2c(PB_9,PB_8);
+
+sonMD            inside(PA_11,PB_2,0.00015);
+sonMD            outside(PA_10,PB_5,0.00015);
+sonMD            inside_reload(PA_9,PA_8,0.00015);
+sonMD            outside_reload(PA_6,PB_6,0.00015);    
+
 DigitalIn        sitalimitswitch(PC_0);
 
 void send(char add, char dat);
 
 int main(){
     raspi.format(8,BufferedSerial::None,1);
-    int res;
-    static char data;
+    clock_t start,end;
+    int res,time;
+    static char data; 
+    float shotpower = 0.9;
+    float reloadpower = 0.5;
+    bool state = false;
     while (true) {
+        printf("shot:%lf,reload:%lf\n",shotpower,reloadpower);
         res = raspi.read(&data,4);
         if(res == 1){
             switch (int(data)){
-                case 7://L1,open wing
-                    if(uelimitswitch == 0){
-                        send(wingPWadd,0xff);
+                case 1://increase shotpower
+                    start = clock();
+                    shotpower += 0.1;
+                    printf("shotpower:%lf\n",shotpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
                     }
+                    break;
+                case 2://decrease shotpower
+                    start = clock();
+                    shotpower -= 0.1;
+                    printf("shotpower:%lf\n",shotpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 3://increase reloadpower
+                    start = clock();
+                    reloadpower += 0.1;
+                    printf("reload:%lf\n",reloadpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 4://decrease reloadpower
+                    start = clock();
+                    reloadpower -= 0.1;
+                    printf("reload:%lf",reloadpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 7://L1,open wing
+                    send(wingPWadd,0xff);
                     break;
                 case 8://L2,close wing
                     if(sitalimitswitch == 0){
@@ -35,30 +88,38 @@ int main(){
                     }
                     break;
                 case 9://maru,inject
-                    inside.move_p1(0.02,shotpower);
-                    outside.move_p1(0.02,shotpower);
-                    leg.move_p1(0.02,shotpower);
+                    state = true;
                     break;
                 case 10://sikaku,reload back
-                    inside_reload.move_p2(0.02,0.2);
-                    outside_reload.move_p2(0.02,0.2);
-                    leg_reload.move_p2(0.02,0.2);
+                    inside_reload.move_p1(0.02,0.5);
+                    outside_reload.move_p1(0.02,0.5);
                     break;
                 case 11://sankaku,reload go
-                    inside_reload.move_p1(0.02,0.1);
-                    outside_reload.move_p1(0.02,0.1);
-                    leg_reload.move_p1(0.02,0.1);
+                    inside_reload.move_p2(0.02,0.5);
+                    outside_reload.move_p2(0.02,0.5);
+                    break;
+                case 12:
+                    state = false;
                     break;
                 default:
                     inside.stop();
                     outside.stop();
-                    leg.stop();
                     inside_reload.stop();
                     outside_reload.stop();
-                    leg_reload.stop();
                     send(wingPWadd,0x80);
                     break;
             }
+            if(state == true){//fir injection
+                inside.move_p1(0.02,shotpower);
+                outside.move_p1(0.02,shotpower);
+            }
+        }
+        else{
+            inside.stop();
+            outside.stop();
+            inside_reload.stop();
+            outside_reload.stop();
+            send(wingPWadd,0x80); 
         }
     }
 }
@@ -68,5 +129,5 @@ void send(char add, char dat){
     i2c.write(add);
     i2c.write(dat);
     i2c.stop();
-    wait_us(15000);
+    wait_us(25000);
 }
--- injection_half_noPW.cpp
+++ injection_half_noPW.cpp
@@ -1,47 +1,105 @@
 #include "mbed.h"
 #include "sonMD.h"
-#define shotpower 0.6
+#include "time.h"
 
-UnbufferedSerial raspi(PA_0,PA_1,9600);
-sonMD            inside(PB_10,PA_8,0.00015);
-sonMD            outside(PA_9,PC_7,0.00015);
-sonMD            inside_reload(PB_6,PB_9,0.00015);
-sonMD            outside_reload(PB_8,PC_9,0.00015);
-sonMD            leg(PC_8,PC_6,0.00015);
-sonMD            leg_reload(PA_11,PB_2,0.00015);
+UnbufferedSerial raspi         (PA_0,PA_1,9600);
+
+sonMD            inside        (PA_8,PB_10,0.00015);
+sonMD            outside       (PA_9,PC_7,0.00015);
+sonMD            inside_reload (PB_6,PB_9,0.00015);
+sonMD            outside_reload(PC_9,PB_8,0.00015);
 
 int main(){
     raspi.format(8,BufferedSerial::None,1);
-    int res; 
+    int res,time;
+    clock_t start,end; 
     static char data;
+    double shotpower = 0.9;
+    double reloadpower = 0.5;
+    bool state = false;
     while(true){    
+        printf("shot:%lf,reload:%lf\n",shotpower,reloadpower);
         res = raspi.read(&data,4);
         if(res == 1){
             switch(int(data)){
+                case 1://increase shotpower
+                    start = clock();
+                    shotpower += 0.1;
+                    printf("shotpower:%lf\n",shotpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 2://decrease shotpower
+                    start = clock();
+                    shotpower -= 0.1;
+                    printf("shotpower:%lf\n",shotpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 3://increase reloadpower
+                    start = clock();
+                    reloadpower += 0.1;
+                    printf("reload:%lf\n",reloadpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
+                case 4://decrease reloadpower
+                    start = clock();
+                    reloadpower -= 0.1;
+                    printf("reload:%lf",reloadpower);
+                    while(true){
+                        end = clock();
+                        time = end - start;
+                        if(double(time)*60/CLOCKS_PER_SEC >= 0.3){
+                            break;
+                        }
+                    }
+                    break;
                 case 9://maru,inject
-                    inside.move_p1(0.02,shotpower);
-                    outside.move_p1(0.02,shotpower);
-                    leg.move_p1(0.02,shotpower);
+                    state = true;
                     break;
                 case 10://sikaku,reload back
-                    inside_reload.move_p2(0.02,0.2);
-                    outside_reload.move_p2(0.02,0.2);
-                    leg_reload.move_p2(0.02,0.2);
+                    inside_reload.move_p2(0.02,reloadpower);
+                    outside_reload.move_p2(0.02,reloadpower);
                     break;
                 case 11://sankaku,reload go
-                    inside_reload.move_p1(0.02,0.1);
-                    outside_reload.move_p1(0.02,0.1);
-                    leg_reload.move_p1(0.02,0.1);
+                    inside_reload.move_p1(0.02,reloadpower);
+                    outside_reload.move_p1(0.02,reloadpower);
+                    break;
+                case 12://batu,stop
+                    state = false;
                     break;
                 default:
                     inside.stop();
                     outside.stop();
                     inside_reload.stop();
                     outside_reload.stop();
-                    leg.stop();
-                    leg_reload.stop();
-                    break;
             }
+            if(state == true){//fir injection
+                inside.move_p1(0.02,shotpower);
+                outside.move_p1(0.02,shotpower);
+            }
+        }
+        else{
+            inside.stop();
+            outside.stop();
+            inside_reload.stop();
+            outside_reload.stop();
         }   
     }
 }
